1ee0894c1a84b9faa8f1044b55e07ce94f47a272
heyyyou <chellymariem01@gmail.com>
2022-11-24 23:37:46 +0100

    master

--- src/main/java/ticketmachine/TicketMachine.java
+++ src/main/java/ticketmachine/TicketMachine.java
@@ -75,7 +75,10 @@ public class TicketMachine {
 	 */
 	public int refund() {
 		System.out.println("Je vous rends : " + balance + " centimes");
-		return balance;
+		int balanceT= balance;
+		balance=0;
+		total=0;
+		return balanceT;
 	}
 
 	/**
@@ -84,6 +87,11 @@ public class TicketMachine {
 	 * @return vrai si le ticket a été imprimé, faux sinon
 	 */
 	public boolean printTicket() {
+		if(balance<price){
+			return false;
+		}
+		balance -=price;
+		total -= balance;
 		if (getPrice() <= getBalance()) {
 			// Simulate the printing of a ticket.
 			System.out.println("##################");
--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -29,4 +29,42 @@ public class TicketMachineTest {
                 // Les montants ont été correctement additionnés  
 		assertEquals(10 + 20, machine.getBalance(), "La balance n'est pas correctement mise à jour");              
 	}
+	@Test
+	//S3 : On n'imprime pas le ticket si le montant inséré est insuffisament.
+	public void nImprimePasSIPasAssezDArgent() {
+		machine.insertMoney(PRICE-1);
+		assertFalse(machine.printTicket(),"Pas  d'argent, pas de ticket");
+	}
+	@Test
+	//S4 On imprime le ticket si le montant inséré est suffisant
+	public void imprimeSiSuffisant(){
+		machine.insertMoney(PRICE);
+		assertTrue(machine.printTicket(), "Ticket Ok");
+	}
+	@Test
+	//S5 Quand on imprime un ticket la balance est décrémentée du prix du ticket
+	public void imprimeEtDecremente(){
+		machine.insertMoney(PRICE+60);
+		machine.printTicket();
+		assertEquals(machine.getBalance(),60);
+	}
+	@Test
+	//S6 Le montant collecté est mis à jour quand on imprime un ticket(pas avant)
+	public void imprimeEtMaJ(){
+		machine.insertMoney(PRICE+60);
+		assertEquals(machine.getBalance(),PRICE+60);
+	}
+	@Test
+	//S7 refund() rend correctement la monnaie
+	public void refundRenduCorrect(){
+		machine.insertMoney(60);
+		assertEquals(machine.refund(),60);
+	}
+	@Test
+	//S8 refund() remet la balance à zéro
+	public void refundRemiseAZero(){
+		machine.insertMoney(60);
+		machine.refund();
+		assertEquals(machine.getBalance(),0);
+	}
 }
